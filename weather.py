6df4aaab79cfc27dab0ffbc8c3c230b81915bc5b
hermda02 <daniher@DESKTOP-2JF6EE8>
2023-09-12 12:41:32 +0200

    now works to add locations using the Add button -- still errors with <Return>

--- weather.py
+++ weather.py
@@ -1,78 +1,56 @@
 import tkinter as tk
 import requests 
 import time
+import datetime
 from appdirs import AppDirs
 import os
 import json
 
-def getWeather(applet):
-    api_key = "822be8f00df6e04cba9aca715e331cc3"
-    city = textfield.get()
-    # try:
-    #     city, country = textfield.get()
-    # except:
-    #     print("Doesn't work")
-    lon_lat_api = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=5&appid="+api_key
-    location_data = requests.get(lon_lat_api).json()
-    lon = float(location_data[0]['lon'])
-    lat = float(location_data[0]['lat'])
-    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key
-    json_data = requests.get(api).json()
+# Simple desktop application made to track weather in a few locations as provided by the user
+# Utilizes tkinter as the main framework. Intended to track just three locations by default
 
+class displayCityWeather():
+    def __init__(self,city,applet):
+        self.cityname=city
+        self.weatherData = ""
 
-    lon_lat_string = 'lon = '+ str(lon) + '\n lat = ' + str(lat)
-    label1.config(text = lon_lat_string)
+        self.cityLabel = tk.Label(applet,text=f"{city}")
+        self.cityLabel.pack()
 
-    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key
-    json_data = requests.get(api).json()
-    condition = json_data['weather'][0]['main']
-    temp = int(json_data['main']['temp'] - 273.15)
-    min_temp = int(json_data['main']['temp_min'] - 273.15)
-    max_temp = int(json_data['main']['temp_max'] - 273.15)
-    pressure = json_data['main']['pressure']
-    humidity = json_data['main']['humidity']
-    wind = json_data['wind']['speed']
-    sunrise = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunrise']))# - 21600))
-    sunset = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunset']))# - 21600))
+        self.condition = tk.Label(applet)
+        self.condition.pack()
 
-    final_info = condition + "\n" + str(temp) + "°C" 
-    final_data = "\n"+ "Min Temp: " + str(min_temp) + "°C" + "\n" + "Max Temp: " + str(max_temp) + "°C" +"\n" + "Pressure: " + str(pressure) + "\n" +"Humidity: " + str(humidity) + "\n" +"Wind Speed: " + str(wind) + "\n" + "Sunrise: " + sunrise + "\n" + "Sunset: " + sunset
+        self.weather = tk.Label(applet)
+        self.weather.pack()
 
-    # return final_data
+    def getWeather(self):
+        
+        api = "https://api.openweathermap.org/data/2.5/weather?q="+self.cityname+"&appid="+api_key
+        json_city_data = requests.get(api).json()
+        self.extractWeather(json_city_data)
 
-    # lon_lat_string = 'lon = '+ str(lon) + '\n lat = ' + str(lat)
-    # label1.configure(text = lon_lat_string)
-    label1.configure(text = final_info)
-    label2.configure(text = final_data)
+    def extractWeather(self,data):#,pars):
 
-def addCity(cities,city):
-    if city in cities:
-        return
-    else:
-        cities.append(city)
+        condition = data['weather'][0]['main']
+        temp = int(data['main']['temp'] - 273.15)
+        min_temp = int(data['main']['temp_min'] - 273.15)
+        max_temp = int(data['main']['temp_max'] - 273.15)
+        pressure = data['main']['pressure']
+        humidity = data['main']['humidity']
+        wind = data['wind']['speed']
+        sunrise = time.strftime('%I:%M:%S', time.gmtime(data['sys']['sunrise']))# - 21600))
+        sunset = time.strftime('%I:%M:%S', time.gmtime(data['sys']['sunset']))# - 21600))
 
-applet = tk.Tk()
-applet.geometry("800x600")
-applet.title("DCH Weather App")
+        final_info = condition + "\n" + str(temp) + "°C" 
+        final_data = "\n"+ "Min Temp: " + str(min_temp) + "°C" + "\n" + "Max Temp: " + str(max_temp) + "°C" +"\n" + "Pressure: " + str(pressure) + "\n" +"Humidity: " + str(humidity) + "\n" + "Wind Speed: " + str(wind) + "\n" + "Sunrise: " + sunrise + "\n" + "Sunset: " + sunset + "\n"
 
-f = ("poppins", 15, "bold")
-t = ("poppins", 35, "bold")
+        self.condition.configure(text=final_info)
+        self.weather.configure(text=final_data)
 
-textfield = tk.Entry(applet, font=t)
-textfield.pack(pady=20)#,side=tk.LEFT)
-textfield.focus()
-textfield.bind('<Return>',getWeather)
+# Global API key for OpenWeather
+api_key = "822be8f00df6e04cba9aca715e331cc3"
 
-label1 = tk.Label(applet, font = t)
-label1.pack(side=tk.LEFT)
-label2 = tk.Label(applet, font = f)
-label2.pack(side=tk.LEFT)
-
-# Define app data locations
-dirs = AppDirs("dch_weather","hermda02")
-
-config_filename = "appdata.json"
-
+# List of all potential weather parameters
 weather_pars = [
     "temp",
     "min_temp",
@@ -84,22 +62,73 @@ weather_pars = [
     "sunset"
 ]
 
-cites = []
+def value(t):
+    x = t.get('1.0','end-1c')
+    return x
 
-# Create dropdwn menu of things to display
-display_menu = tk.OptionMenu(applet, *weather_pars)
-display_menu.pack(side=tk.TOP)
+def getCity(*event):
+    city=value(cityEntry)
+    cities.append(city)
 
-# Weather parameters to be displayed
-display_pars = ""
+def update_clock():
+    # Get current clock time
+    current_time = datetime.datetime.now().strftime("Time: %H:%M:%S")
 
-print(dirs.user_data_dir+'/'+config_filename)
-print(os.path.isfile(dirs.user_data_dir+'/'+config_filename))
+    # And update on the app
+    timelabel.config(text=current_time)
 
-# if os.path.isfile(dirs.user_data_dir+'/'+config_filename):
-#     config = 
+    # run again every 1000 ms
+    applet.after(1000,update_clock)
+
+def update_weather():
+
+    print(cities)
+
+    for city in cities:
+        if city in displayed:
+            continue
+        cityWeather = displayCityWeather(city,applet)
+        cityWeather.getWeather()
+        displayed.append(city)
+        weathers.append(cityWeather)
+
+    # run again every 1000 ms
+    applet.after(1000,update_clock)
+    applet.after(5000,update_weather)
+
+# def refresh_app():
+
+global cities
+global displayed
+global weathers
+
+cities = []
+displayed = []
+weathers = []
+
+config_filename = "appdata.json"
+
+# Define app data locations
+dirs = AppDirs("dch_weather","hermda02")
+
+applet = tk.Tk()
+applet.geometry("800x600")
+applet.title("DCH Weather App")
 
 
+
+timelabel = tk.Label(applet)
+timelabel.pack()
+
+cityEntry = tk.Text(applet, height=1,width=12)
+cityEntry.focus()
+cityEntry.pack()
+cityEntry.bind('<Return>',getCity)
+
+addButton = tk.Button(applet,text="Add",command=getCity).pack()
+
+
+update_weather()
 
 display_locations = []
 
